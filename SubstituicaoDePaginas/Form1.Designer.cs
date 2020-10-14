72092f82ef5e5ce475d26d8ab4cd3b9164644640
Andre Alef Degaspari Moura <alef.moura@verithus.com.br>
2020-10-14 19:19:30 -0300

    first commit

---
+++ SubstituicaoDePaginas/Form1.Designer.cs
@@ -0,0 +1,122 @@
+﻿namespace SubstituicaoDePaginas
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txt = new System.Windows.Forms.TextBox();
+            this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
+            this.txtArray = new System.Windows.Forms.TextBox();
+            this.txtTotal = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // txt
+            // 
+            this.txt.Location = new System.Drawing.Point(28, 99);
+            this.txt.Multiline = true;
+            this.txt.Name = "txt";
+            this.txt.Size = new System.Drawing.Size(189, 313);
+            this.txt.TabIndex = 0;
+            // 
+            // txtArray
+            // 
+            this.txtArray.Location = new System.Drawing.Point(18, 41);
+            this.txtArray.Name = "txtArray";
+            this.txtArray.Size = new System.Drawing.Size(360, 20);
+            this.txtArray.TabIndex = 1;
+            // 
+            // txtTotal
+            // 
+            this.txtTotal.Location = new System.Drawing.Point(119, 418);
+            this.txtTotal.Name = "txtTotal";
+            this.txtTotal.Size = new System.Drawing.Size(71, 20);
+            this.txtTotal.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Arial Narrow", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(13, 18);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(82, 20);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Sequência: ";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Arial Narrow", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(14, 76);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(203, 20);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Páginas | Faltas no momento : ";
+            this.label2.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Arial Narrow", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(24, 418);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(89, 20);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Total Faltas: ";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(428, 463);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtTotal);
+            this.Controls.Add(this.txtArray);
+            this.Controls.Add(this.txt);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txt;
+        private System.ComponentModel.BackgroundWorker backgroundWorker1;
+        private System.Windows.Forms.TextBox txtArray;
+        private System.Windows.Forms.TextBox txtTotal;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ SubstituicaoDePaginas/Form1.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Windows.Forms;
+using System.Linq.Expressions;
+namespace SubstituicaoDePaginas
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+
+            int[] Paginas = new int[5] { 0, 0, 0, 0, 0 };
+            int[] Sequencia = new int[21] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14 , 15, 16, 17, 18, 19, 20, 21};
+            int ContadorPaginas = 0;
+            int Falta = 0;
+            //Array.ForEach(ms, txt.Text);
+            foreach (var item in Sequencia)
+            {
+                txtArray.Text += item.ToString() + ", ";
+            }
+
+            //Laço de repetição pelo tamanho da sequencia
+            for (int i = 0; i < Sequencia.Length; i++)
+            {
+
+                //Verifica se o contador de paginas já está igual o tamanho de paginas
+                if (ContadorPaginas == Paginas.Length)
+                {
+                    //Caso o contador de paginas tenha atingido o numero de paginas, o contador é reiniciado
+                    // e é adicionada uma falta de pagina
+                    ContadorPaginas = 0;
+                    Falta++;   
+                    
+                }
+                //Define que o valor de uma pagina é igual a um numero da sequencia.
+                Paginas[ContadorPaginas] = Sequencia[i];
+
+                // Garante que uma Falta de página não seja contada mais de uma vez
+                if (Falta > 0 && ContadorPaginas != 0)
+                    Falta++;
+
+                if (Falta > 0)
+                    txt.Text += Environment.NewLine + Paginas[0].ToString() + " " + Paginas[1].ToString() + " " + Paginas[2].ToString() + " " 
+                        + Paginas[3].ToString() + " " + Paginas[4].ToString() + "   |   " + Falta.ToString();
+                else
+                    txt.Text += Environment.NewLine + Paginas[0].ToString() + " " + Paginas[1].ToString() + " " + Paginas[2].ToString() + " " +
+                        Paginas[3].ToString()+ " "+ Paginas[4].ToString();
+
+                ContadorPaginas++;
+            }
+
+            //Setar o texto do Total de faltas
+            txtTotal.Text= Falta.ToString();
+
+        }
+
+
+
+    }
+}
